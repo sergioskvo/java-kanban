2853f35384fa414b0341bc86d0cdb2d302621fa3
Sergei Skvortsov <sergioskvor@yandex.ru>
2024-11-17 18:24:39 +0300

    Плюс правка старых тестов после авто проверок в gitHub

--- src/test/model/TaskTest.java
+++ src/test/model/TaskTest.java
@@ -7,23 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskTest {
     @Test
     public void areTwoTaskWithTheSameIdEqual() {
-        Task Task1 = new Task("Task1","Task1");
-        Task1.setIdNumber(1);
-        Task1.setStatus(StatusCodes.NEW);
-        Task Task2 = new Task("Task2","Task2");
-        Task2.setIdNumber(1);
-        Task2.setStatus(StatusCodes.DONE);
-        assertEquals(Task1, Task2);
+        Task task1 = new Task("Task1","Task1");
+        task1.setIdNumber(1);
+        task1.setStatus(StatusCodes.NEW);
+        Task task2 = new Task("Task2","Task2");
+        task2.setIdNumber(1);
+        task2.setStatus(StatusCodes.DONE);
+        assertEquals(task1, task2);
     }
 
     @Test
     public void areTwoTaskWithDifferentIdNotEqual() {
-        Task Task1 = new Task("Task1","Task1");
-        Task1.setIdNumber(2);
-        Task1.setStatus(StatusCodes.NEW);
-        Task Task2 = new Task("Task1","Task1");
-        Task2.setIdNumber(1);
-        Task2.setStatus(StatusCodes.NEW);
-        assertNotEquals(Task1, Task2);
+        Task task1 = new Task("Task1","Task1");
+        task1.setIdNumber(2);
+        task1.setStatus(StatusCodes.NEW);
+        Task task2 = new Task("Task1","Task1");
+        task2.setIdNumber(1);
+        task2.setStatus(StatusCodes.NEW);
+        assertNotEquals(task1, task2);
     }
 }
