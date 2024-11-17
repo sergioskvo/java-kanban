fa0d06337ab3899dd5de0e1b3c13cd3a0c0e0c03
Sergei Skvortsov <sergioskvor@yandex.ru>
2024-11-17 18:46:10 +0300

    Снова чекстайл

--- src/main/controllers/HistoryManager.java
+++ src/main/controllers/HistoryManager.java
@@ -1,9 +1,11 @@
 package controllers;
 
 import model.Task;
+
 import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);
+
     List<Task> getTasks();
 }
--- src/main/controllers/Managers.java
+++ src/main/controllers/Managers.java
@@ -4,6 +4,7 @@ public abstract class Managers {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
+
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
