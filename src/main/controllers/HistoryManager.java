0096b2a11d3e24f231f6de5b13a18ec6a59a636e
Sergei Skvortsov <sergioskvor@yandex.ru>
2024-11-17 18:43:01 +0300

    Убрал лишние пробелы

--- src/main/controllers/HistoryManager.java
+++ src/main/controllers/HistoryManager.java
@@ -5,6 +5,5 @@ import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);
-
     List<Task> getTasks();
 }
--- src/main/controllers/Managers.java
+++ src/main/controllers/Managers.java
@@ -4,7 +4,6 @@ public abstract class Managers {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
-
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
